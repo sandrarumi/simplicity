be8f5d8f70006dd7dd8f56666a840196227df1b8
sandrarumi <91138862+sandrarumi@users.noreply.github.com>
2021-09-21 11:43:52 -0300

    Upload do site Simplicity

---
+++ simplicity/menu.js
@@ -0,0 +1,17 @@
+/*  Evento click para o botao menu, ativa e desativa*/
+const botaoMenu = document.querySelector(".icone"); /* nav h2 a */
+const menu = document.querySelector(".menu"); /* nav ul */
+
+botaoMenu.addEventListener("click", function(event){
+    event.preventDefault();
+    menu.classList.toggle("menu-aberto");
+
+    if( menu.classList.contains("menu-aberto") ){
+        botaoMenu.innerHTML = "Fechar &times;";
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;";
+    }
+});
+
+
+
